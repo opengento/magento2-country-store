f1a871a303747ba93c0ef2d2ec542df148889ef6
Thomas Klein <thomasklein876@gmail.com>
2020-08-01 13:40:14 +0200

    Fix customerData resolver

--- CustomerData/CountryStoreData.php
+++ CustomerData/CountryStoreData.php
@@ -9,24 +9,51 @@ namespace Opengento\CountryStore\CustomerData;
 
 use Magento\Customer\CustomerData\SectionSourceInterface;
 use Magento\Framework\Api\ExtensibleDataObjectConverter;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Store\Model\StoreManagerInterface;
 use Opengento\CountryStore\Api\CountryRegistryInterface;
+use Opengento\CountryStore\Api\CountryStoreResolverInterface;
+use Psr\Log\LoggerInterface;
 
 final class CountryStoreData implements SectionSourceInterface
 {
     private CountryRegistryInterface $countryRegistry;
 
+    private CountryStoreResolverInterface $countryStoreResolver;
+
+    private StoreManagerInterface $storeManager;
+
     private ExtensibleDataObjectConverter $dataObjectConverter;
+
+    private LoggerInterface $logger;
 
     public function __construct(
         CountryRegistryInterface $countryRegistry,
-        ExtensibleDataObjectConverter $dataObjectConverter
+        CountryStoreResolverInterface $countryStoreResolver,
+        StoreManagerInterface $storeManager,
+        ExtensibleDataObjectConverter $dataObjectConverter,
+        LoggerInterface $logger
     ) {
         $this->countryRegistry = $countryRegistry;
+        $this->countryStoreResolver = $countryStoreResolver;
+        $this->storeManager = $storeManager;
         $this->dataObjectConverter = $dataObjectConverter;
+        $this->logger = $logger;
     }
 
     public function getSectionData(): array
     {
+        try {
+            $store = $this->countryStoreResolver->getStoreAware($this->countryRegistry->get());
+            $currentStore = $this->storeManager->getStore();
+
+            if ($store->getCode() !== $currentStore->getCode()) {
+                $this->countryRegistry->clear();
+            }
+        } catch (NoSuchEntityException $e) {
+            $this->logger->error($e->getLogMessage(), $e->getTrace());
+        }
+
         return $this->dataObjectConverter->toFlatArray($this->countryRegistry->get());
     }
 }
